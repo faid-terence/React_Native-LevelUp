fd5439460d067a7ff55b35d3f6b6f7cfacc3912a
Faid Terence JABO <faidterence@outlook.com>
2023-08-10 00:54:46 +0200

    Project SetUp

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image,TouchableNativeFeedback, Button, Alert } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  const handlePress = ()=> Alert.alert("My Title", "My message", [
+    {text: 'Yes', onPress:()=> console.log("Yes")},
+    {text: "No", onPress:()=>console.log("No")},
+  ]);
+  const handleAlert =()=> Alert.prompt("My Title", "My Message", (text)=> console.log(text))
+  return ( 
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello React Native</Text>
+     <TouchableNativeFeedback onPress={handlePress}>
+     <Image source={{
+        width: 200,
+        height: 300,
+        uri:"https://picsum.photos/200/300"}}/>
+     </TouchableNativeFeedback>
+     <Button
+     color="orange"
+      title='Hello Button' onPress={handleAlert}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
