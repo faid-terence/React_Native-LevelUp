534748dd218912a40d8679219f01cd547f65b65e
Faid Terence JABO <faidterence@outlook.com>
2023-08-10 12:41:03 +0200

    feature(Home): Welcome Screen Implemented

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image,TouchableNativeFeedback, Button, Alert } from 'react-native';
-
+import { StyleSheet,Dimensions, Text, View, SafeAreaView, Image,TouchableNativeFeedback, Button, Alert, Platform } from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 export default function App() {
-  const handlePress = ()=> Alert.alert("My Title", "My message", [
-    {text: 'Yes', onPress:()=> console.log("Yes")},
-    {text: "No", onPress:()=>console.log("No")},
-  ]);
-  const handleAlert =()=> Alert.prompt("My Title", "My Message", (text)=> console.log(text))
   return ( 
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello React Native</Text>
-     <TouchableNativeFeedback onPress={handlePress}>
-     <Image source={{
-        width: 200,
-        height: 300,
-        uri:"https://picsum.photos/200/300"}}/>
-     </TouchableNativeFeedback>
-     <Button
-     color="orange"
-      title='Hello Button' onPress={handleAlert}/>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <ViewImageScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
